637aea746267ffc3a98ad84d86ff6e8066398592
Stephanie Chan <stephanietc@gmail.com>
2013-02-10 18:48:30 -0800

    testing git on emacs, little change

--- ccode/RMoveCars.c
+++ ccode/RMoveCars.c
@@ -10,17 +10,7 @@ RupdateBlueCarsCRoutine(int *dimRow,
 			int *ans)
 {
 
-    /* call updateBlueCarsCRoutine with the arguments */
 
-	/*int newBluecars[*numBluecars];
-
-	updateBlueCarsCRoutine(*dimRow,*dimCol,
-								 *numBluecars,bluecars,
-								 *numRedcars,redcars,newBluecars);
-	ans = newBluecars;
-	*/
-
-	updateBlueCarsCRoutine(*dimRow,*dimCol,
 						   *numBluecars,bluecars,
 						   *numRedcars,redcars,ans);
 
